20ee4a9502d0fd572cd4e1c5d67cfa0e07ba14be
radhsyn83 <mocha.fiqri@gmail.com>
2020-02-21 07:12:19 +0700

    new push

--- application/views/kritik.php
+++ application/views/kritik.php
@@ -1,18 +1,60 @@
 <div id="menu-content" class="container h-100 p-0  animated fadeIn">
 	<div class="row align-items-center h-100">
-		<div class="col-md-12 col-12 order-md-2 order-1">
-			<div class="slider-card container-fluid p-0">
-				<div class="card-item-content d-flex flex-row flex-nowrap">
-					<div class="col-12">
-						<div class="card bg-transparent text-white" style="height: 200px">
-							<div class="img-bg h-100 w-auto">
-								<center><span style="padding: 20px 0px 10px 10px; font-size: larger" class="card-title">COMING SOON</span></center>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+		<div class="col-8">
+            <h2 class="text-white">KRITIK DAN SARAN</h2>
+            <h5 class="text-white">Kirimkan kritik dan saran anda untuk unikom semakin maju.</h5>
 		</div>
+        <div class="col-4">
+            <div class="d-flex flex-column shadow text-center body-kritik">
+                <h6 class="text-white">KRITIK DAN SARAN ANDA TERHADAP KEBERSIHAN UNIKOM</h6>
+                <hr style="width: 100%; color: white; height: 1px; background-color: white;" />
+                <form>
+                    <div class="form-group">
+                        <div class="form-row">
+                            <div class="col">
+                                <input type="text" class="form-control" placeholder="Nama">
+                            </div>
+                            <div class="col">
+                                <input type="text" class="form-control" placeholder="Email">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <select class="form-control">
+                            <option value="1">Kebersihan Toilet</option>
+                            <option value="2">Kebersihan Lantai</option>
+                            <option value="3">Kebersihan Ruangan</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Saran">
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-3">
+                                <img src="<?= base_url() ?>assets/images/emot-4.png">
+                                <span class="text-white">Kurang Bersih</span>
+                            </div>
+                            <div class="col-3">
+                                <img src="<?= base_url() ?>assets/images/emot-3.png">
+                                <span class="text-white">Cukup Bersih</span>
+                            </div>
+                            <div class="col-3">
+                                <img src="<?= base_url() ?>assets/images/emot-2.png">
+                                <span class="text-white">Bersih</span>
+                            </div>
+                            <div class="col-3">
+                                <img src="<?= base_url() ?>assets/images/emot-1.png">
+                                <span class="text-white">Sangat Bersih</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group m-0">
+                        <button type="button" class="btn btn-primary btn-sm w-100">Kirim</button>
+                    </div>
+                </form>
+            </div>
+        </div>
 	</div>
 </div>
 
